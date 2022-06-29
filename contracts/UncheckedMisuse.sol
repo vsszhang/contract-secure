9b40abe52bf4317efc30c4c3d5154fe31f6e1dc7
vsszhang <vsszhang@gmail.com>
2022-06-29 17:25:34 +0800

    add uncheckMisuse sample contract

---
+++ contracts/UncheckedMisuse.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract UncheckedMisuse {
+    mapping (address=>uint256) public balance;
+
+    function deposit(uint256 _num) public returns (uint256) {
+        // TODO: add integer overflow situation
+        require(_num <= 2 ** 256 -1, "Your deposit too much number at once!!");
+        balance[msg.sender] += _num;
+        return balance[msg.sender];
+    }
+
+    function withdraw(uint256 _num) public returns (uint256) {
+        unchecked {
+            balance[msg.sender] -= _num;
+        }
+        return balance[msg.sender];
+    }
+}
