8b9f5e6f9b0ebcc64b40591b76f7c2cdcfaf2ae0
vsszhang <vsszhang@gmail.com>
2022-06-30 13:42:49 +0800

    add require check in withdraw func and delete one in deposit

--- contracts/UncheckedMisuse.sol
+++ contracts/UncheckedMisuse.sol
@@ -5,12 +5,12 @@ contract UncheckedMisuse {
     mapping (address=>uint256) public balance;
 
     function deposit(uint256 _num) public returns (uint256) {
-        require(_num <= 2 ** 256 -1, "Your deposit too much number at once!!");
         balance[msg.sender] += _num;
         return balance[msg.sender];
     }
 
     function withdraw(uint256 _num) public returns (uint256) {
+        // require(_num <= balance[msg.sender], "withdraw number must less than your balance");
         unchecked {
             balance[msg.sender] -= _num;
         }
